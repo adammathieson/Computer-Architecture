6afcf90f7d33032bd5f713086fb2e13a9730c11e
Adam Mathieson <mathieson1222@gmail.com>
2019-11-12 15:21:53 -0800

    multiply returns 72

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,7 +8,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 8
-        self.ram = [0] * 6
+        self.ram = [0] * 256
         self.pc = 0
 
     def ram_read(self, address):
@@ -20,23 +20,28 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
+        if len(sys.argv) != 2:
+            print("Usage: comp.py filename")
+            sys.exit(1)
+
         address = 0
 
-        # For now, we've just hardcoded a program:
+        progname = sys.argv[1]
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        with open(progname) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                if line == '':
+                    continue
+                
+                val = int(line, 2)
+                # print(val)
+
+                self.ram[address] = val
+                address += 1
+            # print('end of load')
 
 
     def alu(self, op, reg_a, reg_b):
@@ -45,6 +50,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -73,23 +80,29 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
-        IR = []
         halted = False
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-
+        
         while not halted:
             instruction = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
             if instruction == LDI:
-                print('LDI')
+                self.reg[operand_a] = operand_b
+                print('LDI', self.reg[operand_a])
                 self.pc += 3
 
             elif instruction == PRN:
                 reg_num = self.ram[self.pc + 1]
                 print('PRN', self.ram[reg_num])
                 self.pc += 2
+
+            elif instruction == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                print("MUL", self.reg[operand_a])
+                self.pc += 3
 
             elif instruction == HLT:
                 print('HLT')
